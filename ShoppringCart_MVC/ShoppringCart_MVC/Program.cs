19de9c8b41884dcf8e584b3d9b1d167137a5fff4
JinNguyen-superstar <Phucnguyenn0608@gmail.com>
2025-03-12 15:41:02 +0700

    Test

--- ShoppringCart_MVC/ShoppringCart_MVC/Program.cs
+++ ShoppringCart_MVC/ShoppringCart_MVC/Program.cs
@@ -18,7 +18,7 @@ app.MapStaticAssets();
 
 app.MapControllerRoute(
     name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}")
+    pattern: "{controller=AdminControllers}/{action=Index}/{id?}")
     .WithStaticAssets();
 
 
